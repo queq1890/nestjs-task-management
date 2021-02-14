6ac8da1781a5ea5bace4364af3854f7a9e0a9123
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2021-02-14 19:08:39 +0700

    Save password with salt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,14 +5,17 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialsDto: AuthCredentialsDto) {
     const { username, password } = authCredentialsDto;
+
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = bcrypt.genSaltSync();
+    user.password = this.hashPassword(password, user.salt);
 
     try {
       await user.save();
@@ -24,5 +27,9 @@ export class UserRepository extends Repository<User> {
         throw new InternalServerErrorException();
       }
     }
+  }
+
+  private hashPassword(password: string, salt: string) {
+    return bcrypt.hashSync(password, salt);
   }
 }
